77eb6ee59a0f28d013b5638f3e1542bff4cd73be
rohithseby <48560429+rohithseby@users.noreply.github.com>
2022-10-05 15:02:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const pageHeadings = {
+    MY_WORK : "MY WORK",
+    CONTACT_ME : "CONTACT ME",
+    ABOUT_ME : "ABOUT ME"
+};
+
+let sectionNavigation = document.querySelector('.section-navigation');
+let aboutMeDiv = document.querySelector('#about-me');
+let myWorkDiv = document.querySelector('#my-work');
+let contactDiv = document.querySelector('#contact-me');
+
+let aboutMeButton = document.querySelector('#about-me-button');
+let myWorkButton = document.querySelector('#my-work-button');
+let contactMeButton = document.querySelector('#contact-button');
+let sidebarElement = document.querySelector('#sidebar-bottom-left');
+
+let socialsDiv = document.querySelector('#socials');
+
+aboutMeButton.innerHTML="∎";
+
+
+handleSectionClick = (e) => {
+    if (e.target.id === "about-me-button"){
+        aboutMeDiv.style.display = "inline-block";
+        myWorkDiv.style.display = "none";
+        contactDiv.style.display = "none";
+        aboutMeButton.innerHTML="∎";
+        myWorkButton.innerHTML=pageHeadings.MY_WORK;
+        contactMeButton.innerHTML=pageHeadings.CONTACT_ME;
+    } else if (e.target.id === "my-work-button"){
+        aboutMeDiv.style.display = "none";
+        myWorkDiv.style.display = "inline-block";
+        contactDiv.style.display = "none";
+        aboutMeButton.innerHTML= pageHeadings.ABOUT_ME;
+        myWorkButton.innerHTML="∎";
+        contactMeButton.innerHTML=pageHeadings.CONTACT_ME;
+    } else if (e.target.id === "contact-button"){
+        aboutMeDiv.style.display = "none";
+        myWorkDiv.style.display = "none";
+        contactDiv.style.display = "inline-block";
+        aboutMeButton.innerHTML=pageHeadings.ABOUT_ME;
+        myWorkButton.innerHTML=pageHeadings.MY_WORK;
+        contactMeButton.innerHTML="∎";
+    }
+
+    if (e.target.id === "contact-button"){
+        socialsDiv.style.display = "inline-block";
+        sidebarElement.style.display = "inline-block";
+    } else {
+        socialsDiv.style.display = "none";
+        sidebarElement.style.display = "none";
+    }
+
+    if (e.target.id === "my-work-button"){
+        socialsDiv.style.display = "inline-block";
+    }
+}
+
+
+
+sectionNavigation.addEventListener('click', handleSectionClick);
